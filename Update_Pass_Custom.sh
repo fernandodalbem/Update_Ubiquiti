0ab6dff09e54e896c12112ed59e2db47d4750311
Fernando Junior <fernando.junior@telgo.com.br>
2016-10-26 08:22:05 -0200

    Create Update_Pass_Custom.sh

---
+++ Update_Pass_Custom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cat IP_List_Custom.txt | while read ip ; do
+    host=$(echo $ip | awk '{ print $1 }')
+    user=$(echo $ip | awk '{ print $2 }')
+    pass=$(echo $ip | awk '{ print $3 }')
+    sshpass -p $pass ssh -p22 -oConnectTimeout=10 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null $user@$host "trigger_url https://raw.githubusercontent.com/fernandodalbem/Update_Ubiquiti/master/Update_Ports.sh | sh; reboot" &
+    sleep 0.15
+done
