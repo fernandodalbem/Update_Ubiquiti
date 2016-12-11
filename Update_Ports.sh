8979e4979f655aea85b470a03bccc9fc686975c8
Fernando Junior <fernando.dalbem@outlook.com>
2016-12-11 21:33:42 -0200

    Update Update_Ports.sh

--- Update_Ports.sh
+++ Update_Ports.sh
@@ -17,21 +17,20 @@
 /bin/kill -9 `/bin/pidof mother`
 /bin/kill -9 `/bin/pidof sleep`
 
-sed -ir '/httpd.https.port=443/ c ' /tmp/system.cfg
+sed -ir '/httpd.status=enabled/ c ' /tmp/system.cfg
 sed -ir '/httpd.https.status=enabled/ c ' /tmp/system.cfg
+sed -ir '/httpd.https.port=443/ c ' /tmp/system.cfg
 sed -ir '/httpd.https.port=10443/ c ' /tmp/system.cfg
-sed -ir '/httpd.https.status=disabled/ c ' /tmp/system.cfg
 sed -ir '/sshd.port=22/ c ' /tmp/system.cfg
 sed -ir '/sshd.port=2253/ c ' /tmp/system.cfg
-sed -ir '/httpd.status=enabled/ c ' /tmp/system.cfg
 sed -ir '/httpd.port=80/ c ' /tmp/system.cfg
 sed -ir '/httpd.port=85/ c ' /tmp/system.cfg
 sed -ir 's/users.1.name=ubnt/users.1.name=telgo/g' /tmp/system.cfg
 sed -ir 's/users.1.password=VvpvCwhccFv6Q/users.1.password=PpQsLcRfISTvw/g' /tmp/system.cfg
 
-echo httpd.https.port=10443 >> /tmp/system.cfg
-echo httpd.https.status=disabled >> /tmp/system.cfg
 echo sshd.port=2253 >> /tmp/system.cfg
+echo httpd.https.status=disabled >> /tmp/system.cfg
+echo httpd.https.port=10443 >> /tmp/system.cfg
 echo httpd.status=enabled >> /tmp/system.cfg
 echo httpd.port=85 >> /tmp/system.cfg
 
